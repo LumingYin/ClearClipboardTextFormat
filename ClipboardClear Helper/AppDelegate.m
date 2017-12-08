290bdd4e8276eb019658107eef35895baefa93f5
Luming Yin <vista980622@gmail.com>
2017-12-07 23:40:45 -0500

    Adds auto launcher

---
+++ ClipboardClear Helper/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  ClipboardClear Helper
+//
+//  Created by Numeric on 12/7/17.
+//  Copyright © 2017 Numeric. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    NSArray *pathComponents = [[[NSBundle mainBundle] bundlePath] pathComponents];
+    pathComponents = [pathComponents subarrayWithRange:NSMakeRange(0, [pathComponents count] - 4)];
+    NSString *path = [NSString pathWithComponents:pathComponents];
+    [[NSWorkspace sharedWorkspace] launchApplication:path];
+    [NSApp terminate:nil];
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
--- ClipboardClear/AppDelegate.m
+++ ClipboardClear/AppDelegate.m
@@ -8,16 +8,20 @@
 
 #import "AppDelegate.h"
 #import "ISStatusItemView.h"
+#import <ServiceManagement/ServiceManagement.h>
 
 @interface AppDelegate ()
 @property (nonatomic, strong) NSMenu *rightClickMenu;
 @property (strong, nonatomic) NSStatusItem *statusItem;
+@property (strong, nonatomic) NSMenuItem *toggleLaunchItem;
 @property (strong, nonatomic) ISStatusItemView *statusItemView;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification{
+    
+
     self.statusItemView = [[ISStatusItemView alloc] init];
     self.statusItemView.image = [NSImage imageNamed:@"clipboard"];
     self.statusItemView.target = self;
@@ -26,14 +30,61 @@
     
     self.rightClickMenu = [[NSMenu alloc] init];
     NSMenuItem *clearitem = [[NSMenuItem alloc] initWithTitle:@"Clear Format" action:@selector(clearFormat:) keyEquivalent:@""];
+    self.toggleLaunchItem = [[NSMenuItem alloc] initWithTitle:@"Launch at Login" action:@selector(toggleAutoLoginEnabledDisabled) keyEquivalent:@""];
+    [self.toggleLaunchItem setState:[self launchOnLogin]];
+
     NSMenuItem *quititem = [[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(quit) keyEquivalent:@""];
     [self.rightClickMenu addItem:clearitem];
+    [self.rightClickMenu addItem:self.toggleLaunchItem];
     [self.rightClickMenu addItem:quititem];
     [self.rightClickMenu setDelegate:self];
     
     self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength];
     [self.statusItem setView:self.statusItemView];
 }
+
+- (void)toggleAutoLoginEnabledDisabled {
+    if ([self launchOnLogin] == YES) {
+        if (!SMLoginItemSetEnabled((__bridge CFStringRef)@"com.kay.ClipboardClear-Helper", NO)) {
+            NSLog(@"Login Item Was Not Successfully disabled");
+        } else {
+            NSLog(@"Login Item Was Successfully disabled");
+        }
+        [self.toggleLaunchItem setState:[self launchOnLogin]];
+    } else {
+        if (!SMLoginItemSetEnabled((__bridge CFStringRef)@"com.kay.ClipboardClear-Helper", YES)) {
+            NSLog(@"Login Item Was Not Successfully enabled");
+        } else {
+            NSLog(@"Login Item Was Successfully enabled");
+        }
+        [self.toggleLaunchItem setState:[self launchOnLogin]];
+    }
+}
+
+- (BOOL)launchOnLogin
+{
+    NSArray *jobs = (__bridge NSArray *)SMCopyAllJobDictionaries(kSMDomainUserLaunchd);
+    if (jobs == nil) {
+        return NO;
+    }
+    
+    if ([jobs count] == 0) {
+        CFRelease((CFArrayRef)jobs);
+        return NO;
+    }
+    
+    BOOL onDemand = NO;
+    for (NSDictionary *job in jobs) {
+        if ([@"com.kay.ClipboardClear-Helper" isEqualToString:[job objectForKey:@"Label"]]) {
+            onDemand = [[job objectForKey:@"OnDemand"] boolValue];
+            break;
+        }
+    }
+    
+    CFRelease((CFArrayRef)jobs);
+    return onDemand;
+}
+
 
 - (void)showMenu{
     if(self.statusItemView.clicked){
